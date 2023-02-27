7c21da7d3182737931b3667a6e93534d836836b9
geetaingle <geetaingle321@gmail.com>
2023-02-27 19:37:37 +0530

    Resizable selected region - WIP

--- src/components/ImageSelector.js
+++ src/components/ImageSelector.js
@@ -11,26 +11,56 @@ const ImageSelector = (props) => {
     setStartY(e.nativeEvent.offsetY);
     setEndX(e.nativeEvent.offsetX);
     setEndY(e.nativeEvent.offsetY);
+    console.log(startX, startY, endX, endY);
   };
 
   const handleMouseMove = (e) => {
     if (e.buttons !== 1) return;
-    setEndX(e.nativeEvent.offsetX);
-    setEndY(e.nativeEvent.offsetY);
+    const mouseX = e.nativeEvent.offsetX;
+    const mouseY = e.nativeEvent.offsetY;
+    const width = Math.abs(endX - startX);
+    const height = Math.abs(endY - startY);
+    if (mouseX < startX && mouseY < startY) {
+      setStartX(mouseX);
+      setStartY(mouseY);
+    } else if (mouseX > endX && mouseY < startY) {
+      setEndX(mouseX);
+      setStartY(mouseY);
+    } else if (mouseX < startX && mouseY > endY) {
+      setStartX(mouseX);
+      setEndY(mouseY);
+    } else if (mouseX > endX && mouseY > endY) {
+      setEndX(mouseX);
+      setEndY(mouseY);
+    } else if (mouseX < startX) {
+      setStartX(mouseX);
+    } else if (mouseX > endX) {
+      setEndX(mouseX);
+    } else if (mouseY < startY) {
+      setStartY(mouseY);
+    } else if (mouseY > endY) {
+      setEndY(mouseY);
+    }
+    console.log(startX, startY, endX, endY);
   };
 
   const handleMouseUp = (e) => {
-    console.log(startX, startY, endX, endY);
+    console.log("up");
   };
+
   const selectedRegionStyles = {
     position: "absolute",
     border: "2px solid grey",
     pointerEvent: "none",
+    width: "10px",
+    height: "10px",
+    right: "-6px",
+    bottom: "-6px",
     left: Math.min(startX, endX) + "px",
     top: Math.min(startY, endY) + "px",
     width: Math.abs(endX - startX) + "px",
     height: Math.abs(endY - startY) + "px",
-    backgroundColor: "#808080a1",
+    backgroundColor: "rgba(128, 128, 128, 30%)",
   };
   return (
     <>
@@ -42,7 +72,7 @@ const ImageSelector = (props) => {
           onMouseMove={handleMouseMove}
           onMouseUp={handleMouseUp}
         />
-        <div style={selectedRegionStyles}></div>
+        <div style={{ ...selectedRegionStyles, cursor: "nwse-resize" }} />
       </div>
     </>
   );
