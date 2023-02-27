cd8a130503a13686738ba256062a53e8836b4ee1
geetaingle <geetaingle321@gmail.com>
2023-02-27 16:03:54 +0530

    removed unnecessary file, image added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ImageSelector from './components/ImageSelector';
+import Img from './assets/testplan.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageSelector src={Img} />
     </div>
   );
 }
---
+++ src/components/ImageSelector.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImageSelector = (props) => {
+  return <img src={props.src} alt="testplan" />;
+};
+
+export default ImageSelector;
