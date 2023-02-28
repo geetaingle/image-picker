9fbf27de922f0a1ab74ae14025cec7fd406ef483
geetaingle <geetaingle321@gmail.com>
2023-02-28 19:15:53 +0530

    Display cropped image and details

--- src/components/ImageSelector.js
+++ src/components/ImageSelector.js
@@ -1,98 +1,91 @@
 import React, { useState } from "react";
+import "./ImageSelector.css";
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
 
 const ImageSelector = (props) => {
-  const [startX, setStartX] = useState(0);
-  const [startY, setStartY] = useState(0);
-  const [endX, setEndX] = useState(0);
-  const [endY, setEndY] = useState(0);
+  const [image, setImage] = useState(null);
+  const [crop, setCrop] = useState({ x: 0, y: 0, width: 0, height: 0 });
+  const [croppedImage, setCroppedImage] = useState(null);
 
-  const handleMouseDown = (e) => {
-    setStartX(e.nativeEvent.offsetX);
-    setStartY(e.nativeEvent.offsetY);
-    setEndX(e.nativeEvent.offsetX);
-    setEndY(e.nativeEvent.offsetY);
-    console.log(startX, startY, endX, endY);
-  };
-
-  const handleMouseMove = (e) => {
-    if (e.buttons !== 1)
-      return;
-    const mouseX = e.nativeEvent.offsetX;
-    const mouseY = e.nativeEvent.offsetY;
-    // const width = Math.abs(endX - startX);
-    // const height = Math.abs(endY - startY);
-    if (mouseX < startX && mouseY < startY) {
-      setStartX(mouseX);
-      setStartY(mouseY);
-    } else if (mouseX > endX && mouseY < startY) {
-      setEndX(mouseX);
-      setStartY(mouseY);
-    } else if (mouseX < startX && mouseY > endY) {
-      setStartX(mouseX);
-      setEndY(mouseY);
-    } else if (mouseX > endX && mouseY > endY) {
-      setEndX(mouseX);
-      setEndY(mouseY);
-    } else if (mouseX < startX) {
-      setStartX(mouseX);
-    } else if (mouseX > endX) {
-      setEndX(mouseX);
-    } else if (mouseY < startY) {
-      setStartY(mouseY);
-    } else if (mouseY > endY) {
-      setEndY(mouseY);
+  const onSelectFile = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.addEventListener("load", () => setImage(reader.result));
     }
-
-    console.log(startX, startY, endX, endY);
   };
 
-  const handleMouseMoveinDiv = (e) => {
-    if (e.buttons !== 1)
-      return;
-    const mouseX = e.nativeEvent.offsetX;
-    const mouseY = e.nativeEvent.offsetY;
+  const onCropComplete = (croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea);
+    const canvas = document.createElement("canvas");
 
-
-    if (mouseY + startY > startY && mouseX + startX > startX) {
-      setStartY(mouseY + startY)
-      setStartX(mouseX + startX)
-    }
-    console.log(startX, startY, endX, endY, "div");
-  }
-
-  const handleMouseUp = (e) => {
-    console.log("up");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.src = image;
+    img.onload = () => {
+      ctx.drawImage(
+        img,
+        croppedArea.x,
+        croppedArea.y,
+        croppedArea.width,
+        croppedArea.height,
+        0,
+        0,
+        croppedArea.width,
+        croppedArea.height
+      );
+      console.log(img);
+      setCroppedImage(canvas.toDataURL());
+    };
   };
 
-  const selectedRegionStyles = {
-    position: "absolute",
-    border: "2px solid grey",
-    pointerEvent: "none",
-    right: "-6px",
-    bottom: "-6px",
-    left: Math.min(startX, endX) + "px",
-    top: Math.min(startY, endY) + "px",
-    width: Math.abs(endX - startX) + "px",
-    height: Math.abs(endY - startY) + "px",
-    backgroundColor: "rgba(128, 128, 128, 30%)",
+  const onCropChange = (crop) => {
+    setCrop(crop);
+  };
+
+  const onImageError = (e) => {
+    console.log("Image error:", e);
   };
   return (
-    <>
-      <div style={{ position: "relative" }}>
-        <img
-          src={props.src}
-          alt="testplan"
-          onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-        />
-        <div style={{ ...selectedRegionStyles, cursor: "nwse-resize" }}
-          // onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMoveinDiv}
-        // onMouseUp={handleMouseUp} 
-        />
+    <div className="image-selector-container">
+      <div>
+        <input type="file" accept="image/*" onChange={onSelectFile} />
       </div>
-    </>
+      {image && (
+        <ReactCrop
+          crop={crop}
+          onChange={onCropChange}
+          onComplete={onCropComplete}
+          onError={onImageError}
+        >
+          <img alt="uploadedImg" src={image} />
+        </ReactCrop>
+      )}
+      {croppedImage && (
+        <>
+          <h2 className="heading">New Image: </h2>
+          <div className="cropped-image-container">
+            <img
+              className="cropped-img"
+              src={croppedImage}
+              alt="cropped"
+              style={{}}
+            />
+            <div className="img-details">
+              <div>
+                Start Co-ordinates: ({crop.x},{crop.y})
+              </div>
+              <div>
+                End Co-ordinates: ({crop.x + crop.width},{crop.y + crop.height})
+              </div>
+              <div>Width: {crop.width}px</div>
+              <div>Height: {crop.height}px</div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
